3c07b703f9e56a198465fafe7744a6e0719c8e1d
Andrey Martunyk <aniks.zoom@gmail.com>
2020-05-13 03:25:39 +0300

    Initial commit

---
+++ Questinator2/Controllers/UsersController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Questinator2.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Questinator2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private IUserRepository _userRepo;
+
+        public UsersController(IUserRepository repo)
+        {
+            _userRepo = repo;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> Get()
+        {
+            return new JsonResult( _userRepo.GetUsers());
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id)
+        {
+            User user = _userRepo.GetUsers().FirstOrDefault(x => x.UserId == id);
+            if (user == null)
+                return NotFound();
+            return new JsonResult(user);
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public ActionResult<User> Post(User user)
+        {
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            _userRepo.Create(user);
+            
+            return Ok(user);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public ActionResult<User> Put(User user)
+        {
+            if (user == null)
+            {
+                return BadRequest();
+            }
+            if (!_userRepo.GetUsers().Any(x => x.UserId == user.UserId))
+            {
+                return NotFound();
+            }
+
+            _userRepo.Update(user);
+            
+            return Ok(user);
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public ActionResult<User> Delete(int id)
+        {
+            User user = _userRepo.GetUsers().FirstOrDefault(x => x.UserId == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _userRepo.Delete(user.UserId);
+
+            return Ok(user);
+        }
+    }
+}
---
+++ Questinator2/Startup.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Questinator2.Models;
+
+namespace Questinator2
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connectionString = "Server=(local);Initial Catalog=Questionator;Integrated Security=True";
+            services.AddTransient<IUserRepository, UserRepository>(provider => new UserRepository(connectionString));
+            //сюда будем добавлять остальные сущности из бд
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers(); // подключаем маршрутизацию на контроллеры
+            });
+        }
+    }
+}
